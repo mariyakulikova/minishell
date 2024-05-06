b264bb38416f8df3d36184b597a8e70b28936196
Mariia Kulikova <mkulikov@student.42berlin.de>
2024-05-06 13:09:14 +0200

    add files templates

---
+++ 0_lexer.c
@@ -0,0 +1,36 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   0_lexer.c                                          :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: mkulikov <mkulikov@student.42berlin.de>    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/05/06 11:08:48 by mkulikov          #+#    #+#             */
+/*   Updated: 2024/05/06 13:00:06 by mkulikov         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "minishell.h"
+
+t_tocken	*tocken_new(char **promt)
+{
+	t_tocken	*new;
+
+	return (new);
+}
+
+void	lexer(t_data *data)
+{
+	// char		*promt;
+	// t_tocken	*curr;
+
+	// promt = data->promt;
+	// curr = NULL;
+	// while (*promt)
+	// {
+	// 	if (!curr)
+	// 		curr = tocken_new(&promt);
+	// 	else
+	// 		curr->next = tocken_new(&promt);
+	// }
+}
