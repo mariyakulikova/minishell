05c477b60e9ce027890030b7800b34b06905e57c
mkulikov <mkulikov@student.42berlin.de>
2024-09-11 21:42:11 +0200

    fix test echo 'aspas ->"'

--- srcs/lexer/0_expander.c
+++ srcs/lexer/0_expander.c
@@ -3,10 +3,10 @@
 /*                                                        :::      ::::::::   */
 /*   0_expander.c                                       :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: fjoestin <fjoestin@student.42.fr>          +#+  +:+       +#+        */
+/*   By: mkulikov <mkulikov@student.42berlin.de>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/08/08 12:52:17 by mkulikov          #+#    #+#             */
-/*   Updated: 2024/09/11 19:24:08 by fjoestin         ###   ########.fr       */
+/*   Updated: 2024/09/11 21:32:37 by mkulikov         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -43,8 +43,10 @@ static char	*expand_single_qoutes(char *str, int *i)
 	while (str[++(*i)])
 		if (str[*i] == '\'')
 			break ;
+	if ((size_t)*i == ft_strlen(str))
+		return (str);
 	str = shift_str(str, *i, j);
-	*i -= 1;
+	*i -= 2;
 	return (str);
 }
 
@@ -60,6 +62,8 @@ static char	*expand_double_qoutes(char *str, int *i, t_data *data)
 		if (str[*i] == '\"')
 			break ;
 	}
+	if ((size_t)*i == ft_strlen(str))
+		return (str);
 	str = shift_str(str, *i, j);
 	*i -= 2;
 	return (str);
@@ -69,7 +73,6 @@ void	expander(t_token *tokens, t_data *data)
 {
 	int		i;
 	t_token *curr;
-/* 	t_token *tmp; */
 	char	*str;
 
 	curr = tokens;
@@ -86,38 +89,7 @@ void	expander(t_token *tokens, t_data *data)
 			if (str[i] == '$')
 				str = expand_dollar(str, &i, data);
 		}
-/* 		if (str[0] == '\0')
-		{
-			if (data->n_tokens == 1)
-				ft_exit_err("\n", data);
-			else
-			{
-				if (curr->prev)
-				{
-					tmp = curr->prev;
-					if(curr->next)
-					{
-						curr->next->prev = tmp;
-						tmp->next = curr->next;
-					}
-					else
-						tmp->next = NULL;
-					free(curr);
-					curr = tmp;
-				}
-				else
-				{
-					data->tokens = curr->next;
-					free(curr);
-					curr = data->tokens;
-					curr->prev = NULL;
-				}
-				free(str);
-			}
-		}
-		else */
 			curr->value = str;
 		curr = curr->next;
 	}
-/* 	update_index(data->tokens, data); */
 }
